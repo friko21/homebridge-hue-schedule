c3808d2f4fd879f41d23229e7a0e6771242ad992
friko21 <friedemann.koerner@gmx.de>
2017-01-20 12:58:12 +0100

    Update schedule.js

--- bin/schedule.js
+++ bin/schedule.js
@@ -1,22 +1,27 @@
 var light = require('./light.js');
+var nodeSchedule = require('node-schedule');
 
 var log;
 var name;
 var cron;
 var lights;
+var task;
 
 function Schedule(homebridgeLog, config) {
     this.log = homebridgeLog;
     this.name = config.name;
     this.cron = config.cron;
     for each element in config.lights {
-        var l = new light
-        
-        
+        var l = new light(homebridgeLog, element);
+        this.lights.push(l);
     }
-
+    this.task = nodeSchedule.scheduleJob(cron, execute());
 }
 
+function execute () {
+    
+    
+}
 
 Schedule.prototype = {
 
