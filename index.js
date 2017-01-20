4b4eccf46fc9dd54571f5f0ce0fa49c89cca977d
friko21 <friedemann.koerner@gmx.de>
2017-01-20 07:27:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 var schedule = require('node-schedule');
-var hue = require("node-hue-api");
+var hue = require('node-hue-api');
 
 var Service, Characteristic;
-
+var log;
 
 
 
 module.exports = function (homebridge) {
     Service = homebridge.hap.Service;
     Characteristic = homebridge.hap.Characteristic;
-    homebridge.registerAccessory("homebridge-weather", "Weather", WeatherAccessory);
+    homebridge.registerAccessory('homebridge-hue-schedule', 'Hue-Schedule', HueSchedule);
+}
+
+
+function HueSchedule (homebridgeLog, config) {
+    this.log = homebridgeLog;
+    
+}
+
+
+HueSchedule.prototyp = {
+    
+    identify: function (callback) {
+        log('Identify requested!');
+        callback();
+    },
+    
+    
+    getServices: function () {
+        log('getServices');
+        var informationService = new Service.AccessoryInformation();
+        informationService
+            .setCharacteristic(Characteristic.Manufacturer, 'Hue Schedule')
+            .setCharacteristic(Characteristic.Model, 'Version 0.0.1')
+            .setCharacteristic(Characteristic.SerialNumber, '');
+    
 }
